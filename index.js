0dee6dcecb8ed8df947beb2e2ebce4fe93c65e90
Sem2021 <92226233+Sem2021@users.noreply.github.com>
2021-10-09 15:27:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 function giveMeYourMemory(a) {
     if(a < 2) {
         return a;
- 
     }else{
         return giveMeYourMemory(a-1) + giveMeYourMemory(a - 2);
     }
 }
-giveMeYourMemory(1000000)
+giveMeYourMemory(10000)
